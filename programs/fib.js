04a1ee6a85926ab748051af0bb19e15cb84d7c5e
rockysandeep0909 <rockysandeep0909@gmail.com>
2026-03-21 12:41:21 +0530

    fibonacci , fizzbuzz and SI

---
+++ programs/fib.js
@@ -0,0 +1,9 @@
+let num=10;
+let a=0;
+let b=1;
+for(let i=0;i<=num;i++){
+console.log(a);
+let c=a+b;
+a=b;
+b=c;
+}
---
+++ programs/si.js
@@ -0,0 +1,8 @@
+//Assignment 11: Program to calculate simple interest
+let p = 1000; 
+let t = 4;    
+let r = 8;    
+
+let interest = (p * t * r) / 100;
+
+console.log("Simple Interest:", interest);
