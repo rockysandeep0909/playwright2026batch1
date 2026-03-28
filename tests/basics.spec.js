9e84ff0572fe7d321cc77988578f29e0038fcb54
rockysandeep0909 <rockysandeep0909@gmail.com>
2026-03-28 11:44:39 +0530

    added select dropdown and upload file testcases

--- tests/basics.spec.js
+++ tests/basics.spec.js
@@ -1,15 +1,23 @@
-import {test} from '@playwright/test';
+import {test,expect} from '@playwright/test';
 
-test.only('Login to SauceDemo', async ({page})=>{
+test('Login to SauceDemo', async ({page})=>{
 
 await page.goto("https://www.saucedemo.com/");
+await expect(page).toHaveTitle("Swag Labs");
 await page.locator("//input[@id='user-name']").fill("standard_user");
 await page.locator("//input[@name='password']").fill("secret_sauce");
 await page.locator("//input[@name='login-button']").click();
+await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html")
 console.log("we just completed one simple login flow using playwright - Awesome")
 await page.waitForTimeout(5000);
+await expect(page.locator("//button[@data-test='add-to-cart-sauce-labs-backpack']")).toBeVisible();
 await page.locator("//button[@data-test='add-to-cart-sauce-labs-backpack']").click();
-await page.waitForTimeout(10000);
+await page.waitForTimeout(2000);
+await page.goBack();
+await page.waitForTimeout(2000);
+await page.goForward();
+await page.waitForTimeout(2000);
+await page.reload();
 })
 
 
@@ -19,5 +27,53 @@ await page.goto("https://www.amazon.in/");
 await page.locator("//a[text()='Sell']").click();
 
 await page.waitForTimeout(5000);
+
+})
+
+
+
+test('Paytm navigation', async ({page})=>{
+
+await page.goto("https://paytm.com/");
+await page.pause();
+await page.locator("//li[text()='Ticket Booking']").hover();
+await page.locator("//span[text()='Sign In']").click();
+
+
+
+})
+
+
+test('Selecting different dropdowns', async ({page})=>{
+
+await page.goto("https://www.saucedemo.com/");
+await expect(page).toHaveTitle("Swag Labs");
+await page.locator("//input[@id='user-name']").fill("standard_user");
+await page.locator("//input[@name='password']").fill("secret_sauce");
+await page.locator("//input[@name='login-button']").click();
+await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html")
+console.log("we just completed one simple login flow using playwright - Awesome")
+await page.waitForTimeout(5000);
+
+let dropDown=page.locator("//select[@data-test='product-sort-container']");
+await dropDown.selectOption("hilo");
+await page.waitForTimeout(5000);
+
+await dropDown.selectOption("lohi")
+await page.waitForTimeout(5000);
+await dropDown.selectOption("za");
+await page.waitForTimeout(5000);
+await dropDown.selectOption("az");
+})
+
+
+test.only("File upload for heruko application ", async ({page})=>{
+
+    await page.goto("https://the-internet.herokuapp.com/upload");
+    await page.locator("//input[@id='file-upload']").setInputFiles("C:\\Users\\Sandeep\\Desktop\\sample.txt");
+    await page.locator("//input[@id='file-submit']").click();
+    await page.waitForTimeout(5000);
+    await expect(page.locator("//h3")).toHaveText("ile Uploaded!");
+
 
 })
